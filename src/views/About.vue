34e3c1393e985c65e95b006da55457ffebf08e4c
Axel Knauf <axel.knauf@posteo.de>
2018-06-29 16:35:37 +0200

    Added BootstrapVue and made 1/3-2/3 layout. Hiding still missing.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,30 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <b-container id="about">
+    <b-row>
+      <b-col class="col-4">
+        <div class="about-left">
+          <h1>This is the left third.</h1>
+        </div>
+      </b-col>
+      <b-col class="col-8">
+        <div class="about-right">
+          <h1>These are the right two thirds.</h1>
+        </div>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
+
+<style lang="scss">
+#about {
+  border: 1px solid green;
+}
+
+.about-left {
+  color: red;
+}
+
+.about-right {
+  color: blue;
+}
+</style>
